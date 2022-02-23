6ba03c1c2b205fb4ee3c68f3651f1d97fe1987a3
ShriAnubala <shrivastavaanubala@gmail.com>
2022-02-23 13:42:32 -0800

    Trial 31

--- cypress/integration/Test/Test-2.spec.js
+++ cypress/integration/Test/Test-2.spec.js
@@ -10,10 +10,11 @@ context('Network Requests', () => {
   
     it('cy.request() - make an XHR request', () => {
         cy.log(' first it block')
-        var recordkey = Cypress.env.RECORD_KEY
-        //  var user = Cypress.env('SE_CYPRESS_LOGIN')
+        var recordkey = Cypress.env().RECORD_KEY
+        var user = Cypress.env().se_login
         //  var pass = Cypress.env('SE_CYPRESS_PASSWORD')
         //  var acaCookie = Cypress.env('SE_IMP_ACA_COOKIE')
+        expect (user).to.have('Anu')
          expect(recordkey).to.have.string('AAA')
         //  if ( recordkey !== 'AAA111'){
         //     expect(true).to.be.eql(false);
