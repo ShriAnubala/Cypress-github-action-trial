7e379f3c6d3779bdedfd46fe876b3cc7a85ca623
ShriAnubala <shrivastavaanubala@gmail.com>
2022-02-22 20:18:41 -0800

    Trial 17

--- cypress/integration/Test/Test-1.spec.js
+++ cypress/integration/Test/Test-1.spec.js
@@ -2,12 +2,14 @@
 
 context('Cypress.Commands', () => {
     beforeEach(() => {
-      cy,log('running before each from test 1')
+      cy.log('running before each from test 1')
     })
   
     // https://on.cypress.io/custom-commands
   
     it('.add() - create a custom command', () => {
+        cy.log(Cypress.config().baseUrl)
+        cy.visit(Cypress.config().baseUrl)
 
       })
 
